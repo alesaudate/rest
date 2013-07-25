a08563091247deff5d781b0c8425c7b334b22507
Alexandre Saudate <alesaudate@gmail.com>
2013-07-25 11:29:42 -0300

    JSON schema

--- cap-04/src/main/java/br/com/brejaonline/parsers/XStreamParser.java
+++ cap-04/src/main/java/br/com/brejaonline/parsers/XStreamParser.java
@@ -2,7 +2,9 @@ package br.com.brejaonline.parsers;
 
 import br.com.brejaonline.modelo.pessoa.PessoaFisica;
 
+import com.thoughtworks.xstream.MarshallingStrategy;
 import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.core.ReferenceByIdMarshallingStrategy;
 import com.thoughtworks.xstream.io.json.JettisonMappedXmlDriver;
 
 public class XStreamParser {
@@ -13,7 +15,11 @@ public class XStreamParser {
 	public static void main(String[] args) {
 		PessoaFisica pessoaFisica = Parser.criarPessoaFisicaTeste();
 		
+		JettisonMappedXmlDriver jettisonMappedXmlDriver = new JettisonMappedXmlDriver();
+		
+		
 		XStream xStream = new XStream(new JettisonMappedXmlDriver());
+		
 		
 		System.out.println(xStream.toXML(pessoaFisica));
 
