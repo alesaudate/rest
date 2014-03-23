78bdb3901d08aaac32a65ee84cc814bae4dcc849
Alexandre Saudate <alesaudate@gmail.com>
2014-03-23 19:25:40 -0300

    rename de pacotes

--- cap-06/src/main/java/br/com/brejaonline/model/rest/package-info.java
+++
@@ -1,7 +0,0 @@
-
-
-@XmlJavaTypeAdapter(value=JaxbAdapter.class)
-package br.com.brejaonline.model.rest;
-
-import javax.ws.rs.core.Link.JaxbAdapter;
-import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
